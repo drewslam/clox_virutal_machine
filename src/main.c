4f33b721c514018842aa93ea22b7ff1bfb4d2d9d
Drewslam <souzaphonix@pm.me>
2024-10-28 19:31:23 -0700

    Chapter 16

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ static char* readFile(const char* path) {
     size_t fileSize = ftell(file);
     rewind(file);
 
-    char* buffer = (char*)malloc(filesize + 1);
+    char* buffer = (char*)malloc(fileSize + 1);
     if (buffer == NULL) {
         fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
         exit(74);
